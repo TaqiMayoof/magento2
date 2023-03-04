bfe620fc8990f1fc2dbccf0882cde27a4a2bd373
TaqiMayoof <Taqi_99@outlook.com>
2023-03-04 12:58:05 +0300

    added sample data & created child theme based on the luma theme

---
+++ app/design/frontend/FirstTheme/luma_child77/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/FirstTheme/luma_child77', __DIR__);
